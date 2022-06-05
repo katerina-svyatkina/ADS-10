13fd73f55fdfe6c65f5ee7f7defc366e74538170
Екатерина Святкина <99070196+katerina-svyatkina@users.noreply.github.com>
2022-06-05 18:28:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,10 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+  std::string sour = tree[n - 1];
+    std::vector<char> tree1;
+    for (int g = 0; g < sour.length(); g++) {
+        tree1.push_back(sour[g]);
+    }
+    return tree1;
 }
